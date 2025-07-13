8c93519f11ca63e3be15fe457ad424b8ab59e6f5
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-12 22:07:28 -0400

    Adding references between projects

--- ReportManager.Application/Services/ReportService.cs
+++ ReportManager.Application/Services/ReportService.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReportManager.Application.DTOs;
+using ReportManager.Application.Interfaces;
+using ReportManager.Domain.Entities;
+using ReportManager.Domain.Interfaces;
 
 namespace ReportManager.Application.Services
 {
