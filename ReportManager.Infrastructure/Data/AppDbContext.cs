5ec3a9d0e5129c3b234a45af1bcb47f46a44ca2b
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-14 20:46:12 -0400

    Adding AppDbContext

--- ReportManager.Infrastructure/Data/AppDbContext.cs
+++ ReportManager.Infrastructure/Data/AppDbContext.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ReportManager.Domain.Entities;
 
 namespace ReportManager.Infrastructure.Data
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext (DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Report> Reports => Set<Report>();
     }
 }
