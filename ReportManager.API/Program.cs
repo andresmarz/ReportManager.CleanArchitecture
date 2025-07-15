1db194bf9299e2852073f49058323c91a9c2ae32
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-15 12:25:17 -0400

    Adding the dependency injection on Program.cs

--- ReportManager.API/Program.cs
+++ ReportManager.API/Program.cs
@@ -14,6 +14,9 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+// Dependency injection
+builder.Services.AddScoped<IReportRepository, ReportRepository>();
+builder.Services.AddScoped<IReportService, ReportService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
