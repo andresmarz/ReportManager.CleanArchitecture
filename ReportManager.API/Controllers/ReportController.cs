c36b43e8cd86162c6b562ea181c38e7d85d952f3
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-17 21:45:03 -0400

    Adding GetAll and GetById methods on ReportController

--- ReportManager.API/Controllers/ReportController.cs
+++ ReportManager.API/Controllers/ReportController.cs
@@ -2,6 +2,8 @@
 using ReportManager.Application.DTOs;
 using ReportManager.Application.Interfaces;
 
+namespace ReportManager.API.Controllers;
+
 [ApiController]
 [Route("api/[controller]")]
 public class ReportController : ControllerBase
@@ -13,6 +15,25 @@ public class ReportController : ControllerBase
         _reportService = reportService;
     }
 
+    //GET: api/Report
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var reports = await _reportService.GetReportsAsync();
+        return Ok(reports);
+    }
+
+
+    //GET: api/report/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var report = await _reportService.GetReportByIdAsync(id);
+        if (report == null) return NotFound();
+        return Ok(report);
+    }
+
+
     [HttpPost]
     public async Task<IActionResult> CreateReport(CreateReportDto dto)
     {
@@ -20,10 +41,5 @@ public class ReportController : ControllerBase
         return Ok();
     }
 
-    [HttpGet]
-    public async Task<IActionResult> GetReports()
-    {
-        var reports = await _reportService.GetReportsAsync();
-        return Ok(reports);
-    }
+    
 }
