9673aa87aa299f893fca72272998af41b33d10ff
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-20 00:19:01 -0400

    Adding IAuthService interface

---
+++ ReportManager.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReportManager.Application.DTOs;
+using ReportManager.Application.Interfaces;
+
+namespace ReportManager.Application.Interfaces
+{
+    public class IAuthService
+    {
+        string GenerateToken(string username);
+        Task<bool> ValidateUserLogin(LoginDto loginDto);
+    }
+}
