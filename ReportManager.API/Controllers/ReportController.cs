ed51758992043deae6c9da380f153fe0b9a82ec3
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-21 17:51:53 -0400

    Adding Attribute Authorize to ReportController

--- ReportManager.API/Controllers/ReportController.cs
+++ ReportManager.API/Controllers/ReportController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ReportManager.Application.DTOs;
 using ReportManager.Application.Interfaces;
 
 namespace ReportManager.API.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class ReportController : ControllerBase
