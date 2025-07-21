d16cc2f4d42da674e830e2b012479207158aa69b
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-21 12:47:05 -0400

    Adding AuthController

---
+++ ReportManager.API/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReportManager.Application.DTOs;
+using ReportManager.Application.Interfaces;
+
+namespace ReportManager.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        // POST: api/Auth/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var isValid = await _authService.ValidateUserAsync(loginDto);
+
+            if (!isValid)
+                return Unauthorized(new { message = "Invalid credentials" });
+
+            var token = _authService.GenerateToken(loginDto.Username);
+
+            return Ok(new { token });
+        }
+    }
+}
